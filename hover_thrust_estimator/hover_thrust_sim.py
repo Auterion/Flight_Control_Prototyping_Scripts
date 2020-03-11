9f82ffd8aceeb24e092e784e1e6cf26e81d936a3
bresch <brescianimathieu@gmail.com>
2020-03-11 08:28:12 +0100

    hover_thrust_sim: update test

--- hover_thrust_estimator/hover_thrust_sim.py
+++ hover_thrust_estimator/hover_thrust_sim.py
@@ -27,24 +27,20 @@ def getAccelFromThrTime(thrust, t, ht):
     return accel
 
 def getThrFromTime(t):
-    if t < 0.6:
-        thrust = t
-
-    elif t < 1.5:
-        thrust = 0.6
-
-    elif t < 2.5:
+    if t <= 10.0:
+        thrust = 0.8
+    else:
         thrust = 0.5
 
-    elif t < 4.0:
-        thrust = 0.5 - (t - 2.5) * 0.25
-
-    elif t < 6.0:
-        thrust = 0.42
-    else:
-        thrust = 0.8
-
     return thrust
+
+def getHoverThrustFromTime(t):
+    if t <= 10.0:
+        hover_t = 0.8
+    else:
+        hover_t = 0.5
+
+    return hover_t
 
 
 if __name__ == '__main__':
@@ -56,66 +52,94 @@ if __name__ == '__main__':
 
 
     # Estimator initial conditions
-    hover_thr_0 = 0.5
-    hover_thr_noise_0 = 0.2
-    P0 = hover_thr_noise_0**2
-    hover_thr_process_noise = 0.01 # hover thrust change / s
-    Q = hover_thr_process_noise**2
-    Qk = Q * dt
-    accel_noise = 3.5e-1 # in m/s2
-    R = accel_noise**2 # Rk = R
+    hover_thrust_0 = 0.5
+    hover_thrust_noise_0 = 0.1
+    P0 = hover_thrust_noise_0**2
+    hover_thrust_process_noise = sqrt(0.25e-6) # hover thrust change / s
+    Q = hover_thrust_process_noise**2
+    Qk = Q
+    accel_noise_0 = sqrt(5.0)
+    R = accel_noise_0**2 # Rk = R
 
-    hover_ekf = HoverThrEstimator(hover_thr_0)
+    hover_ekf = HoverThrEstimator(hover_thrust_0)
     hover_ekf.setStateVar(P0)
     hover_ekf.setProcessVar(Qk)
     hover_ekf.setMeasVar(R)
+
+    accel_noise_sim = 2.0 # in m/s2
+    hover_thrust_sim = 0.8
 
     # Create data buckets
     accel = zeros(n)
     accel_true = zeros(n)
     thrust = ones(n)
-    hover_thr = zeros(n)
-    hover_thr_std = zeros(n)
-    hover_thr_true = zeros(n)
-    accel_noise_estimated = zeros(n)
+    hover_thrust = zeros(n)
+    hover_thrust_std = zeros(n)
+    hover_thrust_true = zeros(n)
+    accel_noise_std = zeros(n)
+    innov_test_ratio_lpf = zeros(n)
+    innov = zeros(n)
+    innov_std = zeros(n)
+    innov_test_ratio = zeros(n)
+    residual_lpf = zeros(n)
 
     for k in range(0, n):
         # Save data
-        hover_thr[k] = hover_ekf._hover_thr
-        hover_thr_std[k] = sqrt(hover_ekf._P)
+        hover_thrust[k] = hover_ekf._hover_thr
+        hover_thrust_std[k] = sqrt(hover_ekf._P)
 
         # Generate measurement
         thrust[k] = getThrFromTime(t[k])
-        noise = random.randn() * accel_noise
-        hover_thr_true[k] = 0.42 + 0.2 * (t[k]/t_end)
-        accel_true[k] = getAccelFromThrTime(thrust[k], t[k], hover_thr_true[k])
+        noise = random.randn() * accel_noise_sim
+        hover_thrust_true[k] = getHoverThrustFromTime(t[k])
+        accel_true[k] = getAccelFromThrTime(thrust[k], t[k], hover_thrust_true[k])
         accel[k] =  accel_true[k] + noise
 
         # Accel noise change
         if t[k] > 40.0:
-            accel_noise = 0.4
+            accel_noise_sim = 2.0
         elif t[k] > 30.0:
-            accel_noise = 2.5
+            accel_noise_sim = 4.0
 
         # Update the EKF
         hover_ekf.predict(dt)
-        hover_ekf.fuseAccZ(accel[k], thrust[k])
-        accel_noise_estimated[k] = sqrt(hover_ekf._R)
+        (innov[k], innov_var, innov_test_ratio[k]) = hover_ekf.fuseAccZ(accel[k], thrust[k])
+        innov_std[k] = sqrt(innov_var)
+        accel_noise_std[k] = sqrt(hover_ekf._R)
+        innov_test_ratio_lpf[k] = hover_ekf._innov_test_ratio_lpf
+        residual_lpf[k] = hover_ekf._residual_lpf
         # print("P = ", hover_ekf._P, "\tQ = ", hover_ekf._Q, "\tsqrt(R) = ", sqrt(hover_ekf._R))
 
     # Plot results
-    ax1 = plt.subplot(2, 1, 1)
+    n_plots = 5
+    ax1 = plt.subplot(n_plots, 1, 1)
     ax1.plot(t, thrust * 10.0, '.')
     ax1.plot(t, accel, '.')
-    ax1.plot(t, accel_true + accel_noise_estimated, 'g--')
-    ax1.plot(t, accel_true - accel_noise_estimated, 'g--')
+    ax1.plot(t, accel_true + 3*accel_noise_std, 'g--')
+    ax1.plot(t, accel_true - 3*accel_noise_std, 'g--')
     ax1.legend(["Thrust (10x)", "AccZ"])
 
-    ax2 = plt.subplot(2, 1, 2, sharex=ax1)
-    ax2.plot(t, hover_thr, 'b')
-    ax2.plot(t, hover_thr_true, 'k:')
-    ax2.plot(t, hover_thr + hover_thr_std, 'g--')
-    ax2.plot(t, hover_thr - hover_thr_std, 'g--')
-    plt.legend(["Ht_Est", "Ht_True"])
+    ax2 = plt.subplot(n_plots, 1, 2, sharex=ax1)
+    ax2.plot(t, hover_thrust, 'b')
+    ax2.plot(t, hover_thrust_true, 'k:')
+    ax2.plot(t, hover_thrust + hover_thrust_std, 'g--')
+    ax2.plot(t, hover_thrust - hover_thrust_std, 'g--')
+    ax2.legend(["Ht_Est", "Ht_True"])
+
+    ax3 = plt.subplot(n_plots, 1, 3, sharex=ax1)
+    ax3.plot(t, hover_thrust_std)
+    ax3.legend(["Ht_noise"])
+
+    ax4 = plt.subplot(n_plots, 1, 4, sharex=ax1)
+    ax4.plot(t, innov_test_ratio)
+    ax4.plot(t, innov_test_ratio_lpf)
+    ax4.plot(t, residual_lpf)
+    ax4.legend(["Test ratio", "Test ratio lpf", "Residual lpf"])
+
+    ax5 = plt.subplot(n_plots, 1, 5, sharex=ax1)
+    ax5.plot(t, innov)
+    ax5.plot(t, innov + innov_std, 'g--')
+    ax5.plot(t, innov - innov_std, 'g--')
+    ax5.legend(["Innov"])
 
     plt.show()
